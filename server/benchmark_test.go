b4384a52ddc48870264e58694f763f087b3dcf9f
Dan Webb <dan.webb@damacus.io>
2026-04-15 09:28:43 +0100

    test: remove stale readiness benchmark

--- server/benchmark_test.go
+++ server/benchmark_test.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"net/http"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
@@ -23,33 +20,6 @@ func BenchmarkIsReady_NoIO(b *testing.B) {
 	// Outside Kubernetes case
 	kubernetesServiceHost = ""
 	kubernetesServicePort = ""
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = isReady()
-	}
-}
-
-func BenchmarkIsReady_WithIO(b *testing.B) {
-	// In-cluster case (simulated)
-	kubernetesServiceHost = "10.0.0.1"
-	kubernetesServicePort = "443"
-	httpClient = &http.Client{}
-
-	tmpDir, err := os.MkdirTemp("", "hp-bench")
-	if err != nil {
-		b.Fatal(err)
-	}
-	defer os.RemoveAll(tmpDir)
-
-	origTokenPath := tokenPath
-	tokenPath = filepath.Join(tmpDir, "token")
-	defer func() { tokenPath = origTokenPath }()
-
-	err = os.WriteFile(tokenPath, []byte("some-token-value"), 0644)
-	if err != nil {
-		b.Fatal(err)
-	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
